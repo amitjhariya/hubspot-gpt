b1d0b5849e70d5d49afb525c630ce23d5b30e159
Ravi.Kumar <ravi.kumar@arm.com>
2023-06-11 19:00:53 +0530

    Remove .env

--- controllers/chat-controller.js
+++ controllers/chat-controller.js
@@ -6,31 +6,9 @@ const hubspotClient = new Client({ accessToken: 'pat-na1-824773c4-aad4-45f0-934d
 
 
 export const testChat = async (req, res, next) => {
-  const chatData = req.body;
+  const data = req.body;
   try {
-    console.log(chatData)
-    const url = 'https://api.hubspot.com/conversations/v3/visitor-identification/tokens/create';
-    const data = {
-      email: 'gob@bluth.com',
-      firstName: 'Gob',
-      lastName: 'Bluth'
-    };
-  
-    const options = {
-      method: 'POST',
-      headers: {
-        'Authorization': `Bearer pat-na1-824773c4-aad4-45f0-934d-d3ecf27c1a43`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    };
-
-    const response = await fetch(url, options);
-    const result = await response.json();
-
-    // const data = await fetch('/oauth/v2/private-apps/get/access-token-info')
-    // const data = await Chat.create(chatData);
-    res.status(201).json({ success: true, result });
+    res.status(201).json({ success: true, data,success:"GPT Success" });
   } catch (error) {
     next(error);
   }
